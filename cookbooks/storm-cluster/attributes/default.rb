fdcf13ddfff3739b6b09abb6195316f4840ec4bb
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-09-22 15:43:14 +0200

    Remove stale Apache Storm recipe
    Change-Id: I33fa89f0dbeb1547e69e086c4cc639eb2b23478f

--- cookbooks/storm-cluster/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default['storm']['version'] = '1.0.1'
-default['storm']['package'] = "apache-storm-#{node['storm']['version']}"
-default['storm']['install_dir'] = '/usr/share/storm'
-default['storm']['user'] = 'storm'
-
-default['storm']['install_method'] = 'remote_file'
-
-default['storm']['download_url'] = 'http://www.apache.si/storm'
-default['storm']['download_dir'] = "/#{node['storm']['package']}/#{node['storm']['package']}.tar.gz"
