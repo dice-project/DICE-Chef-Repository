234773f3fa34395ee54edf4a4c8922c6d9c6f746
Matej Artač <matej.artac@xlab.si>
2016-09-29 09:25:48 +0200

    Bump the dmon and dmon_agent versions
    Change-Id: I3eb85ee78c2aeed690d67de5cdc6fa2027c2a48b

--- cookbooks/dmon/metadata.rb
+++ cookbooks/dmon/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robert.cvitkovic@xlab.si'
 license 'all_rights'
 description 'Installs/Configures dmon'
 long_description 'Installs/Configures dmon'
-version '0.1.0'
+version '0.1.1'
 
 depends 'apt', '~> 2.9.1'
 depends 'git', '~> 4.0.2'
--- cookbooks/dmon_agent/metadata.rb
+++ cookbooks/dmon_agent/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robert.cvitkovic@xlab.si'
 license 'all_rights'
 description 'Installs/Configures dmon_agent'
 long_description 'Installs/Configures dmon_agent'
-version '0.1.0'
+version '0.1.1'
 
 depends 'apt', '~> 2.9.1'
 depends 'poise-python', '~>1.4.0'
